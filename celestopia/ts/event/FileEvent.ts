1e8b45e70355c7fa8b7f07aa11517c4d953ef4ca
Frisk202020 <156703513+Frisk202020@users.noreply.github.com>
2025-08-28 14:36:09 +0200

    reworked form build for save and load + implemented database menu (need to implement file display)

--- celestopia/ts/event/FileEvent.ts
+++ celestopia/ts/event/FileEvent.ts
@@ -1,4 +1,3 @@
-import { appendBlurryBackground, appendCross } from "../util/functions.js";
 import { sendLoadRequest, sendSaveRequest } from "../util/save.js";
 import { BoardEvent } from "./BoardEvent.js";
 
@@ -14,7 +13,7 @@ export class FileEvent extends BoardEvent {
         super(
             buttons,
             BoardEvent.unappendedOkSetup(),
-            BoardEvent.denySetup(false)
+            BoardEvent.denySetup(true, "Retour")
         )
     }
 }
@@ -30,45 +29,141 @@ class Message extends BoardEvent {
     }
 }
 
-class SaveEvent {
+class SaveEvent extends BoardEvent {
     constructor() {
         removeOldMenu();
 
-        const menu = appendBlurryBackground();
-        menu.style.display = "flex";
-        menu.style.flexDirection = "column";
-        menu.style.alignItems = "center"
-        menu.innerHTML = `
-            <p style="font-size: 50px; text-align: center;">Sauvegarder</p>
-            <form id="saveForm" action="/celestopia/save" method="post" autocomplete="off">
-                <label for="name">Nom du fichier: </label><br>
-                <input name="name" type="text"><br><br>
-                <input type="submit" value="Ok">
-            </form>
-        `;
-        appendCross(["menu"], menu);
-        document.getElementById("saveForm")?.addEventListener("submit", saveHandler)
+        const form = createForm(
+            {
+                name: "saveForm",
+                entries: [{name: "name", label: "Nom de fichier"}],
+                submitLabel: "Ok"
+            }
+        );
+        form.addEventListener("submit", saveHandler);
+
+        super(
+            [BoardEvent.generateTextBox("Sauvegarder"), form],
+            BoardEvent.unappendedOkSetup(),
+            BoardEvent.denySetup(true, "Retour", ()=>new FileEvent())
+        )
     }
 }
 
-class LoadEvent {
+
+class SaveKey extends BoardEvent {
+    constructor(filename: string) {
+        removeOldMenu();
+
+        const p = BoardEvent.generateTextBox("Entrez la clé d'authentification de ce fichier.");
+        p.id = "message"; 
+
+        const form = createForm(
+            {
+                name: "saveForm",
+                entries: [{name: "key", label: "Clé", id: "formInput"}],
+                submitLabel: "Ok"
+            }
+        );
+        form.addEventListener("submit", (event)=>authentification_handler(filename, event))
+
+        super(
+            [p, form],
+            BoardEvent.unappendedOkSetup(),
+            BoardEvent.denySetup(true, "Retour", ()=>new FileEvent())
+        )
+    }
+}
+
+class LoadEvent extends BoardEvent {
     constructor() {
         removeOldMenu();
 
-        const menu = appendBlurryBackground();
-        menu.style.display = "flex";
-        menu.style.flexDirection = "column";
-        menu.style.alignItems = "center"
-        menu.innerHTML = `
-            <p style="font-size: 50px; text-align: center;">Charger</p>
-            <form id="saveForm" action="/celestopia/save" method="post" autocomplete="off">
-                <label for="name">Nom du fichier: </label><br>
-                <input name="name" type="text"><br><br>
-                <input type="submit" value="Ok">
-            </form>
-        `;
-        appendCross(["menu"], menu);
-        document.getElementById("saveForm")?.addEventListener("submit", loadHandler)
+        const form = createForm(
+            {
+                name: "loadForm",
+                entries: [{name: "name", label: "Nom de fichier"}],
+                submitLabel: "Ok"
+            }
+        );
+        form.addEventListener("submit", loadHandler);
+
+        super(
+            [BoardEvent.generateTextBox("Charger"), form],
+            BoardEvent.okSetup(true, "Afficher les fichiers disponibles", ()=>new DataEvent()),
+            BoardEvent.denySetup(true, "Retour", ()=>new FileEvent())
+        )
+    }
+}
+
+class DataEvent extends BoardEvent {
+    #box: HTMLDivElement;
+    constructor() {
+        removeOldMenu();
+        
+        const box = document.createElement("div");
+        box.style.display = "flex";
+        box.style.flexDirection = "column";
+        box.style.overflowY = "scroll";
+        box.style.width = "60vw";
+        box.style.height = "60vh";
+        box.style.backgroundColor = "#faf088";
+        box.style.borderRadius = "10px";
+        box.style.alignItems = "center";
+
+        super(
+            [BoardEvent.generateTextBox("Fichiers disponibles"), box],
+            BoardEvent.unappendedOkSetup(),
+            BoardEvent.denySetup(true, "Retour", ()=>new LoadEvent())
+        )
+
+        this.#box = box;
+        this.loadData();
+    }
+
+    async loadData() {
+        const data = await sendDataRequest();
+        if (data.length === 0) {
+            this.#box.appendChild(BoardEvent.generateTextBox("Aucun fichier n'a été trouvé..."));
+            return;
+        }
+
+        data.forEach((x)=>this.#appendFileBox(x));
+    }
+
+    #appendFileBox(name: string) {
+        const box = document.createElement("div");
+        box.textContent = name;
+        box.style.width = "50vw";
+        box.style.backgroundColor = "#ffd700";
+        box.style.borderRadius = "10px";
+        box.style.display = "grid";
+        box.style.alignItems = "center";
+        box.style.textAlign = "center";
+        box.style.margin = "1vh";
+        box.style.padding = "1vh";
+        box.style.fontSize = "x-large";
+        box.className = "pointerHover";
+        box.addEventListener("mouseenter", ()=>box.classList.add("hue"));
+        box.addEventListener("mouseleave", ()=>box.classList.remove("hue"));
+
+        this.#box.appendChild(box);
+    }
+}
+
+async function sendDataRequest() {
+    try {
+        const res = await fetch("celestopia/database");
+        if (!res.ok) {
+            console.log(`Request failed: ${res.body}`);
+            return [];
+        }
+
+        const out: string[] = await res.json();
+        return out;
+    } catch (err) {
+        console.log(`Caught error on data request: ${err}`);
+        return [];
     }
 }
 
@@ -98,17 +193,7 @@ async function saveHandler(event: SubmitEvent) {
             new Message(messages);
         } else {
             if (res.authentification_error) {
-                const elm = document.getElementById("menu") as HTMLDivElement;
-                elm.innerHTML = `
-                    <p id="message" style="font-size: 30px; text-align: center;">Entrez la clé d'authentification de ce fichier.</p>
-                    <form id="saveForm" action="/celestopia/save" method="post" autocomplete="off">
-                        <label for="key">Clé: </label><br>
-                        <input name="key" type="text" id="formInput"><br><br>
-                        <input type="submit" value="Ok">
-                    </form>
-                `;
-                appendCross(["menu"], elm);
-                document.getElementById("saveForm")?.addEventListener("submit", (event)=>authentification_handler(litteralName, event))
+                new SaveKey(litteralName);
             } else {
                 new Message(["Sauvegarde échouée, veuillez réessayer avec un autre nom."]);
             }
@@ -137,4 +222,44 @@ async function authentification_handler(file: string, event: SubmitEvent) {
 function removeOldMenu() {
     const old = document.getElementById("menu");
     if (old !== null) { document.body.removeChild(old); }
+}
+
+function createForm(builder: FormBuilder) {
+    const form = document.createElement("form");
+    form.id = builder.name;
+    form.method = "post";
+    form.autocomplete = "off";
+
+    builder.entries.forEach((x)=>{
+        const label = document.createElement("label");
+        label.htmlFor = x.name;
+        label.textContent = x.label;
+        form.appendChild(label);
+        form.appendChild(document.createElement("br"));
+
+        const input = document.createElement("input");
+        input.name = x.name;
+        input.type = "text";
+        if (x.id !== undefined) { input.id = x.id; }
+        form.appendChild(input);
+        for (let i = 0; i < 2; i++) { form.appendChild(document.createElement("br")); }
+    });
+
+    const submit = document.createElement("input");
+    submit.type = "submit";
+    submit.value = builder.submitLabel;
+    form.appendChild(submit);
+    return form;
+}
+
+interface FormBuilder {
+    name: string,
+    entries: FormEntryBuilder[],
+    submitLabel: string
+}
+
+interface FormEntryBuilder {
+    name: string,
+    label: string,
+    id?: string
 }
