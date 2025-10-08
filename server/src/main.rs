e3ffd46d8d0ddc92d74ec212bda7cc7d34c6422d
Frisk202020 <156703513+Frisk202020@users.noreply.github.com>
2025-10-08 14:08:25 +0200

    removed log files

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,19 +4,19 @@ mod celestopia;
 mod log;
 
 use std::{
-    env, fs::OpenOptions, 
+    //env, fs::OpenOptions, 
     net::{
         //IpAddr, 
         SocketAddr
     }
 };
 use axum::{response::Redirect, routing, Router};
-use chrono::Utc;
+// use chrono::Utc;
 use tokio::task::JoinHandle;
 use tower::ServiceBuilder;
 use tracing::{error, info};
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer, Registry};
-use util::*;
+//use util::*;
 use anyhow::Result;
 use tower_http::{cors::{Any, CorsLayer}, services::ServeDir};
 
@@ -24,6 +24,7 @@ use crate::{celestopia::{list, load, save}, log::{get_latest_log, get_log_handle
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    /*
     let now = Utc::now().format(FORMAT).to_string();
     let path = correct_path(
         env::current_exe().inspect_err(|_| println!("Failed to get current exe path"))?, 
@@ -34,12 +35,13 @@ async fn main() -> Result<()> {
         .write(true)
         .create_new(true)
         .open(&path).inspect_err(|_| println!("Failed to create log file at: {}", path.display()))?;
+    */
 
     Registry::default()
         .with(
             fmt::layer()
                 .json()
-                .with_writer(log_file)
+                //.with_writer(log_file)
                 .with_filter(EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("TRACE")))
         ).init();
 
@@ -62,7 +64,7 @@ async fn main() -> Result<()> {
 
             .route("/lost-in-the-void", routing::get(Redirect::permanent("/lost-in-the-void/")))
             .nest_service("/lost-in-the-void/", service("../lost-in-the-void"))  
-            .nest_service("/test/", service("../test"))
+            //.nest_service("/test/", service("../test"))
             .nest_service("/fez/", service("../fez"))
             .route("/fez", routing::get(Redirect::permanent("/fez/")))
             .layer(
